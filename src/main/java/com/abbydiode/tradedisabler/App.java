0cfa1006bc676efb6bc8c1e8b4078362b0f60239
Abby <abbydiode@gmail.com>
2022-01-23 17:27:24 +0100

    Add config

--- src/main/java/com/abbydiode/tradedisabler/App.java
+++ src/main/java/com/abbydiode/tradedisabler/App.java
@@ -7,6 +7,8 @@ import com.abbydiode.tradedisabler.listeners.PlayerInteractEntityListener;
 public class App extends JavaPlugin {
 	@Override
 	public void onEnable() {
+		saveDefaultConfig();
+
 		new PlayerInteractEntityListener(this);
 	}
 }
